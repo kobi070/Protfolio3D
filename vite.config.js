dce48eff6efff4b6b275fae1103fddbd42427ce7
kobi070 <kobi070@gmail.com>
2024-07-12 12:04:10 +0300

    #Update: Updating to use Pages in github

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://github.com/kobi070/Protfolio3D.git',
   plugins: [react()],
   assetsInclude: ['**/*.glb']
 })
