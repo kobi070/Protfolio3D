0950dd5d814cc06c4f6e9a1831cf9a1c71424214
kobi070 <kobi070@gmail.com>
2024-07-14 10:35:18 +0300

    fixed buging with github pages

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,77 @@
-import React from 'react'
+import { useState } from 'react'
 
 const Contact = () => {
+  const [form, setform] = useState({ name: '', email: '', message: '' })
+  const [isLoading, setisLoading] = useState(false)
+  const handleChange = () => { };
+  const handleFocused = () => { };
+  const handleBlur = () => { };
+
   return (
-    <div>Contact</div>
+    <section className='relative flex lg:flex-row flex-col
+    max-container'>
+      <div className='flex-1 flex flex-col'>
+        <h1 className='sm:text-5xl text-3xl font-extrabold sm:leading-snug font-manrope'>
+          Get in Touch
+        </h1>
+      </div>
+      <form className='w-full flex flex-col gap-7 mt-14'>
+        <label className='text-black-500'>
+          Name
+          <input
+            type='text'
+            name='name'
+            className='input'
+            placeholder='John'
+            required
+            value={form.name}
+            onChange={handleChange}
+            onFocus={handleFocused}
+            onBlur={handleBlur}>
+          </input>
+        </label>
+
+        <label className='text-black-500'>
+          Email
+          <input
+            type='email'
+            name='email'
+            className='input'
+            placeholder='john@gmail.com'
+            required
+            value={form.email}
+            onChange={handleChange}
+            onFocus={handleFocused}
+            onBlur={handleBlur}>
+          </input>
+        </label>
+
+        <label className='text-black-500'>
+          Your Message
+          <textarea
+            name='message'
+            row={4}
+            className='textarea'
+            placeholder='Let me know how I can help you!'
+            required
+            value={form.message}
+            onChange={handleChange}
+            onFocus={handleFocused}
+            onBlur={handleBlur}>
+          </textarea>
+        </label>
+
+        <button
+          type='submit'
+          className='btn'
+          disabled={isLoading}
+          onFocus={handleFocused}
+          onBlur={handleBlur}>
+          {isLoading ? 'Sending...' : 'Send Message'}
+        </button>
+
+      </form>
+    </section>
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/kobi070/Protfolio3D.git',
+  base: '/',
   plugins: [react()],
   assetsInclude: ['**/*.glb']
 })
