6fbbb64e43af0a3e1232842b49f015345f705f6a
kobi070 <kobi070@gmail.com>
2024-07-14 12:13:11 +0300

    #Update: addition of Fox model to the page && minor bug fixing

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,8 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, Suspense } from 'react'
 import emailjs from '@emailjs/browser'
+import { Canvas } from '@react-three/fiber';
+import Fox from '../models/Fox';
+import Loader from '../components/Loader'
 
 const Contact = () => {
 
@@ -53,66 +56,85 @@ const Contact = () => {
         <h1 className='sm:text-5xl text-3xl font-extrabold sm:leading-snug font-manrope'>
           Get in Touch
         </h1>
+        <form
+          ref={formRef}
+          className='w-full flex flex-col gap-7 mt-14'
+          onSubmit={handleSubmit}>
+          <label className='text-black-500'>
+            Name
+            <input
+              type='text'
+              name='name'
+              className='input'
+              placeholder='John'
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocused}
+              onBlur={handleBlur}>
+            </input>
+          </label>
+
+          <label className='text-black-500'>
+            Email
+            <input
+              type='email'
+              name='email'
+              className='input'
+              placeholder='john@gmail.com'
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocused}
+              onBlur={handleBlur}>
+            </input>
+          </label>
+
+          <label className='text-black-500'>
+            Your Message
+            <textarea
+              name='message'
+              row={4}
+              className='textarea'
+              placeholder='Let me know how I can help you!'
+              required
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocused}
+              onBlur={handleBlur}>
+            </textarea>
+          </label>
+
+          <button
+            type='submit'
+            className='btn'
+            disabled={isLoading}
+            onFocus={handleFocused}
+            onBlur={handleBlur}>
+            {isLoading ? 'Sending...' : 'Send Message'}
+          </button>
+
+        </form>
       </div>
-      <form
-        ref={formRef}
-        className='w-full flex flex-col gap-7 mt-14'
-        onSubmit={handleSubmit}>
-        <label className='text-black-500'>
-          Name
-          <input
-            type='text'
-            name='name'
-            className='input'
-            placeholder='John'
-            required
-            value={form.name}
-            onChange={handleChange}
-            onFocus={handleFocused}
-            onBlur={handleBlur}>
-          </input>
-        </label>
+      <div className='lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]'>
+        <Canvas
+          camera={{
+            position: [0, 0, 5],
+            fov: 75,
+            near: 0.1,
+            far: 1000
+          }}>
+          <directionalLight intensity={2.5} position={[0,0,1]} />
+          <ambientLight intensity={0.6} />
+          <Suspense fallback={<Loader />}>
+            <Fox
+              position={[0.5, 0.35, 0]}
+              rotation={[12.625, -0.625, 0]}
+              scale={[0.55, 0.55, 0.55]} />
+          </Suspense>
 
-        <label className='text-black-500'>
-          Email
-          <input
-            type='email'
-            name='email'
-            className='input'
-            placeholder='john@gmail.com'
-            required
-            value={form.email}
-            onChange={handleChange}
-            onFocus={handleFocused}
-            onBlur={handleBlur}>
-          </input>
-        </label>
-
-        <label className='text-black-500'>
-          Your Message
-          <textarea
-            name='message'
-            row={4}
-            className='textarea'
-            placeholder='Let me know how I can help you!'
-            required
-            value={form.message}
-            onChange={handleChange}
-            onFocus={handleFocused}
-            onBlur={handleBlur}>
-          </textarea>
-        </label>
-
-        <button
-          type='submit'
-          className='btn'
-          disabled={isLoading}
-          onFocus={handleFocused}
-          onBlur={handleBlur}>
-          {isLoading ? 'Sending...' : 'Send Message'}
-        </button>
-
-      </form>
+        </Canvas>
+      </div>
     </section>
   )
 }
