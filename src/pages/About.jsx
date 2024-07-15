304a79988445d85d01d37895d6aac3f9f74e3faf
kobi070 <kobi070@gmail.com>
2024-07-15 10:07:28 +0300

    Created About component with skills display:
    - Imported React and skills data from constants.
    - Defined the About component structure, including a personal introduction and skills section.
    - Added HTML and CSS classes for styling the section, heading, and text.
    - Implemented a dynamic rendering of skills using the map function to iterate through the skills array.
    - Each skill is displayed in a styled container with an image representing the skill.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import { skills } from '../constants'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='max-container'>
+      <h1 className='head-text'> Hello, I'm <span className='blue-gradient_text
+       font-semibold drop-shadow'>
+        Kobi</span>
+      </h1>
+
+      <div className='mt-5 felx flex-col gap3 text-slate-500'>
+        <p>
+          Software Developer with a B.Sc, in Computer Science based in Israel.
+          passionate about building innovative software solutions.
+          I specialize in both front-end and back-end development, database management, and DevOps practices.
+        </p>
+      </div>
+
+      <div className='py-10 flex flex-col'>
+        <h3 className='subhead-text'>My Skills</h3>
+        <div className='mt-16 flex flex-wrap gap-12'>
+          {
+            skills.map((skill) => (
+              <div className='block-container w-20 h-20'>
+              <div className='btn-back rounded-x1'/>
+                <div className='btn-front rounded-xl flex 
+                justify-center items-center'>
+                  <img
+                    src={skill.imageUrl}
+                    alt={skill.name}
+                    className='w-1/2 h-1/2 object-contain'>
+
+                  </img>
+                </div>
+              </div>
+            )
+            )
+          }
+        </div>
+      </div>
+
+    </section>
   )
 }
 
